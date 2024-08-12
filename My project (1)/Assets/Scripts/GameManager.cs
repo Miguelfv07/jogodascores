0491d6f8e5969803ef7d730d5d96c1b08610b7b1
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-08-12 09:11:09 -0300

    Script UIManager

--- My project (1)/Assets/Scripts/GameManager.cs
+++ My project (1)/Assets/Scripts/GameManager.cs
@@ -1,18 +1,35 @@
+
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    int corDaVez, acertos, erros;
+    public int[] sequencia;
+    [SerializeField]public string[] nomes;
+    public static GameManager instance;
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        GerarSequencia();
     }
+
+ public void GerarSequencia()
+    {
+        corDaVez = 0;
+        sequencia =   
+        UIManager.instance.LimparTexto();
+        for(int i = 0; i < sequencia.Length;i++)
+        {
+            sequencia[i] = Random.Range(0, nomes.Length);
+            UIManager.instance.AtualizarSequencia();
+        }
+    }
+    
 }
